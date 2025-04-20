4b2e0ad346298bd9eb4a4f70f9bf3a915548f6d4
cody <24554738+codywill@users.noreply.github.com>
2025-04-19 18:59:50 -0700

    Install tweaks

--- install.sh
+++ install.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+sudo echo ""
+
 install_packages() {
     pkgs=(build-essential cmake fd-find lua5.1 liblua5.1-dev ripgrep stow tmux unzip)
+    sudo apt-get update && \
     sudo apt-get -y --ignore-missing install "${pkgs[@]}"
 }
 
 install_neovim() {
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-    sudo rm -rf /opt/nvim
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && \
+    sudo rm -rf /opt/nvim && \
     sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 }
 
 install_starship() {
-    curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh | sh -s -- "-y"
 }
 
 install_fish() {
-    sudo apt-add-repository ppa:fish-shell/release-4
-    sudo apt update
-    sudo apt install fish
+    sudo apt-add-repository -y ppa:fish-shell/release-4 && \
+    sudo apt update && \
+    sudo apt install -y fish
 }
 
 install_tpm() {
@@ -26,12 +29,12 @@ install_tpm() {
 }
 
 install_luarocks() {
-    wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
-    tar zxpf luarocks-3.11.1.tar.gz
-    pushd luarocks-3.11.1
-    ./configure && make && sudo make install
-    popd
-    rm -rf luarocks-3.11.1
+    wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz && \
+    tar zxpf luarocks-3.11.1.tar.gz && \
+    pushd luarocks-3.11.1 && \
+    ./configure && make && sudo make install && \
+    popd && \
+    rm -rf luarocks-3.11.1 && \
     rm luarocks-3.11.1.tar.gz
 }
 
@@ -45,5 +48,5 @@ install_starship
 install_fish
 install_tpm
 install_luarocks
-install_dotfiles
+install_dotfiles 
 
