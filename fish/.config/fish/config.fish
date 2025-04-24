1e5dc828a281c5f4d2c6682a9c106bdc25c4c4fe
Cody <codywill77@gmail.com>
2025-04-23 17:01:36 -0700

    Added hotkeys, cleaned up indents (#4)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 end
+
+function fish_user_key_bindings
+    bind \cy forward-char
+end
