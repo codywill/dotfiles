c4c0bf36b0b07118ef5fa2a45c2d50931517c742
Cody Williams <codywill77@gmail.com>
2025-06-04 21:10:48 -0700

    Updated terminal style, cleaned up

--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,3 +1,15 @@
 return {
-    'mrcjkb/rustaceanvim', version = '^6'
+    'mrcjkb/rustaceanvim',
+    version = '^6',
+    init = function()
+        vim.g.rustaceanvim = {
+            server = {
+                default_settings = {
+                    ['rust-analyzer'] = {
+                        check = { command = 'clippy', },
+                    },
+                },
+            },
+        }
+    end,
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 
--- local is_darwin = wezterm.target_triple:find("darwin") ~= nil
+wezterm.log_info(wezterm.target_triple)
+
 local is_linux = wezterm.target_triple:find("linux") ~= nil
 local is_windows = wezterm.target_triple:find("windows") ~= nil
 
@@ -14,14 +15,32 @@ if is_windows then
     config.default_domain = 'WSL:Ubuntu'
     config.window_decorations = "TITLE | RESIZE"
 elseif is_linux then
-    config.window_decorations = "RESIZE"
+    config.window_decorations = "NONE"
 end
 
--- Config
+-- Default program
 config.default_prog = { 'tmux' }
 
 -- Style
-config.font_size = 13.0
+local default_font_size = 14
+local font_size = default_font_size
+local resolution_to_font_size = {
+    ["3840x2160"] = 14,
+    ["2560x1440"] = 12.5,
+    ["1920x1200"] = 13.1,
+}
+
+local function recompute_font_size(window)
+    local dimensions = window:get_dimensions()
+    local resolution = string.format("%dx%d", dimensions.pixel_width, dimensions.pixel_height)
+    local overrides = window:get_config_overrides() or {}
+
+    overrides.font_size = resolution_to_font_size[resolution]
+
+    window:set_config_overrides(overrides)
+end
+
+config.font_size = font_size
 config.font = wezterm.font({
     family = 'VictorMono Nerd Font',
     style = 'Normal',
@@ -30,15 +49,22 @@ config.font = wezterm.font({
 config.color_scheme = 'Atelier Plateau (base16)'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-    left = '1cell',
-    right = '1cell',
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
 }
+config.window_background_opacity = 0.96
+config.initial_rows = 40
+config.initial_cols = 150
 
 wezterm.on('format-window-title', function()
     local title = ''
     return title
+end)
+
+wezterm.on('window-resized', function(window)
+    recompute_font_size(window)
 end)
 
 return config
